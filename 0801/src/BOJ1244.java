5eddcccc5ca19dd86faaa7cbba80f9eb62adf916
jey0623 <jey0623@naver.com>
2022-08-01 23:02:21 +0900

    220801_algo

---
+++ 0801/src/BOJ1244.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1244 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(bf.readLine()); //스위치 개수
+		
+	}
+	
+}
