08fc24b3a373918ac3015b5f63d83bd3dd96a08f
rlooo <jey0623@naver.com>
2022-11-07 17:53:20 +0900

    1107_algo

--- src/Main_2573_빙산.java
+++ src/Main_2573_빙산.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_2573_빙산 {
     static int[][][] map;
-    static boolean[][] visited;
-    static Queue<Pair> q3;
-
+    static boolean[][] visited_bfs;
+    static List<Pair> ice_list = new LinkedList<>();
+    static boolean[][] visited_dfs;
+    static int N, M;
     static int[] dx = { 0, 0, -1, 1 };
     static int[] dy = { -1, 1, 0, 0 };
 
@@ -32,101 +35,92 @@ public class Main_2573_빙산 {
         StringBuilder sb = new StringBuilder();
 
         st = new StringTokenizer(in.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
         map = new int[N][M][2];
-        Queue<Pair> q = new ArrayDeque<>();
-        int ice = 0;
+        visited_bfs = new boolean[N][M];
+        visited_dfs = new boolean[N][M];
+
+        Pair start = null;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(in.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j][0] = Integer.parseInt(st.nextToken());
-                if (map[i][j][0] != 0) {
-                    q.add(new Pair(i, j));
-                    ice++;
-                }
-            }
-        }
-        Queue<Pair> q2 = new ArrayDeque<>();
-        while (!q.isEmpty()) {
-            Pair cur = q.poll();
-            q2.add(cur);
-
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
-
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-                    continue;
-
-                if (map[nx][ny][0] == 0) {
-                    map[nx][ny][1] += 1;
-                }
-            }
-        }
-
-        int cnt = 1;
-        q3 = new ArrayDeque<>();
-        while (cnt < 2) {
-            boolean flag = false;
-            while (!q2.isEmpty()) {
-                Pair cur = q2.poll();
-                q3.add(cur);
-
-                map[cur.x][cur.y][0] -= map[cur.x][cur.y][1];
-                if (map[cur.x][cur.y][0] < 0)
-                    map[cur.x][cur.y][0] = 0;
-                if (map[cur.x][cur.y][0] == 0) {
-                    flag = true;
-                    ice--;
-                }
-                if (ice == 0) {
-                    sb.append(-1);
-                    break;
+                if (map[i][j][0] > 0) {
+                    start = new Pair(i, j);
                 }
 
             }
-            // 영역 구하기(빙산 중 높이가 0이 되는게 있을 때에만)
-            if (flag == true) {
-                visited = new boolean[N][M];
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < M; j++) {
-                        if (visited[i][j] != true) {
-                            bfs(new Pair(i, j));
-                            cnt++;
-                        }
+        }
+        bfs(start);
 
-                    }
-                }
+        // while (true) {
+        // 전체 빙산에 대한 주변 바다 개수 구하기
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(map[i][j][1] + " ");
             }
+            System.out.println();
+        }
+        // 높이 줄이기
+        for (int i = 0; i < ice_list.size(); i++) {
+            int x = ice_list.get(i).x;
+            int y = ice_list.get(i).y;
+            map[x][y][0] -= map[x][y][1];
+        }
 
-        }
-        if (cnt != 1) {
-            sb.append(cnt);
-        }
+        // }
 
         out.write(sb.toString());
         out.flush();
         out.close();
     }
 
-    private static void bfs( Pair start) {
-        q3.add(start);
-        visited[start.x][start.y] = true;
-        while (!q3.isEmpty()) {
-            Pair cur = q3.poll();
+    private static void bfs(Pair start) {
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(start);
+        visited_bfs[start.x][start.y] = true;
+
+        while (!q.isEmpty()) {
+            Pair cur = q.poll();
             for (int dir = 0; dir < 4; dir++) {
                 int nx = cur.x + dx[dir];
                 int ny = cur.y + dy[dir];
 
                 if (nx < 0 || ny < 0 || nx >= N || ny >= M)
                     continue;
-                if (visited[nx][ny] != false)
+                if (map[nx][ny][0] == 0) {
+                    map[cur.x][cur.y][1]++;
                     continue;
-                visited[nx][ny] = true;
-                q3.add(new Pair(nx, ny));
+                }
+                if (visited_bfs[nx][ny] != false)
+                    continue;
+                visited_bfs[nx][ny] = true;
+                ice_list.add(new Pair(nx, ny));
+                q.add(new Pair(nx, ny));
 
             }
+
+        }
+    }
+
+    private static void dfs(Pair cur) {
+
+        visited_bfs[cur.x][cur.y] = true;
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+                continue;
+            if (visited_dfs[nx][ny] != false)
+                continue;
+            if (map[nx][ny][0] == 0) {
+
+                continue;
+            }
+
         }
 
     }
