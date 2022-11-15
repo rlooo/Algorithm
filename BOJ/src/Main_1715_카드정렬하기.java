81e6c121efde197dffb367a820bc97502e9f69fc
jey0623 <jey0623@naver.com>
2022-11-16 02:07:47 +0900

    1116_algo

--- BOJ/src/Main_1715_카드정렬하기.java
+++ BOJ/src/Main_1715_카드정렬하기.java
@@ -3,6 +3,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_1715_카드정렬하기 {
@@ -13,6 +15,25 @@ public class Main_1715_카드정렬하기 {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(in.readLine());
+
+        Queue<Integer> q = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            q.add(Integer.parseInt(in.readLine()));
+        }
+
+        int sum = 0;
+        while (q.size() > 1) {
+            int a = q.poll();
+            sum += a;
+
+            int b = q.poll();
+            sum += b;
+            q.add(a + b);
+        }
+        sb.append(sum);
+        out.write(sb.toString());
+        out.flush();
+        out.close();
 
     }
 
